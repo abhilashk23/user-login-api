5bd3118dfd5eed2dd218e478e23fa47519129b5f
abhilashk23 <abhilashgurukul23@gmail.com>
2023-08-12 13:07:09 +0530

    Basic register path added

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); 
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
+const app = express();
+const PORT = 5000;
+
+const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.CLUSTER}.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>{
+    console.log("Connected to db");
+})
+.catch((e) =>{
+    console.log(e);
+});
+
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,POST',
+    credentials: true,
+  }));
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.use('/users', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/userRoutes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const multer = require('multer');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/register', upload.single('profileImage'), async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email already exists' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+      profileImage: req.file ? req.file.filename : null,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering user' });
+  }
+});
+
+module.exports = router;
